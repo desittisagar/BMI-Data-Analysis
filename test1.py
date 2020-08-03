c73c145b0e9807a90669db3255119e493e0c13a1
desittisagar <dsagar.smart@gmail.com>
2020-08-03 15:48:21 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,227 @@
+import matplotlib.pyplot as plt
+
+life_exp = []
+bmi_men_dict = {}
+bmi_women_dict = {}
+
+f = open("life.csv", "r")
+life=f.read()
+life1=life.split("\n")[1:-1]
+for i in range(len(life1)):
+    x=life1[i].split(",")
+    for j in range(1, len(x)):
+        x[j] = float(x[j])
+    life_exp.append(x)
+
+f1 = open("bmi_men.csv", "r")
+men=f1.read()
+men1=men.split("\n")[1:-1]
+men_list=[]
+for i in range(len(men1)):
+    x=men1[i].split(",")
+    for j in range(1, len(x)):
+        x[j] = float(x[j])
+    men_list.append(x)
+for i in range(len(men_list)):
+    key, value = men_list[i][0], men_list[i][1:]
+    bmi_men_dict[key] = value
+   
+f2 = open("bmi_women.csv", "r")
+women=f2.read()
+women1=women.split("\n")[1:-1]
+
+
+women_list=[]
+for i in range(len(women1)):
+    x=women1[i].split(",")
+    for j in range(1, len(x)):
+        x[j] = float(x[j])
+    women_list.append(x)
+for i in range(len(women_list)):
+    key, value = women_list[i][0], women_list[i][1:]
+    bmi_women_dict[key] = value
+
+#2 - making of bmi_all - avg of men and women
+
+bmi_all = {}
+try_bmi = []
+
+
+for i, j in bmi_men_dict.items():
+    x1 = []
+    for x, y in bmi_women_dict.items():
+        if i == x:    
+            bmi_all[i]=res_list = [round((j[k] + y[k])/2,2) for k in range(29)]
+            #print(res_list)
+            # print(i)
+            # print(res_list)
+            x1.append(i)
+            #print(x1)
+            x1.extend(res_list)
+            #print(x1)
+            try_bmi.append(x1)
+            #print(try_bmi)
+            #x1.clear()
+
+ 
+
+print(try_bmi[0][1])   
+print(try_bmi[0:][4])  
+
+for i in range(0,12):
+    print(try_bmi[i][0])
+
+for i in range(0,12):
+    print(try_bmi[0][i])
+
+
+
+#print(len(try_bmi))       
+
+#3 - min, max and median of bmi values in a user given year
+
+while True:
+    year = (input("Select a year to find statistics (1980 to 2008): "))
+    if year.isdigit() and int(year)>=1980 and int(year)<=2008:
+        pos = int(year) - 1980
+        min_val = min(list(try_bmi[:][pos]))
+        max_val = max(list(try_bmi[:][pos]))
+        temp_list = list(try_bmi[:][pos])
+        temp_list.sort()
+        min_country = ''
+        max_country = ''
+
+        for i in range(0,186):
+            if min_val == try_bmi[i][pos]:
+                min_country = try_bmi[i][0]
+            if max_val == try_bmi[i][pos]:
+                max_country = try_bmi[i][0]
+
+        print(f"\nIn {year}, countries with minimum and maximum BMI values were '{min_country}' and '{max_country}' and Median BMI value in {year} was {round(((temp_list[92]+temp_list[93])/2.000),5)}")
+        print("\n\n")
+        break
+    else:
+        print("<error> That is an invalid year.")
+        continue
+
+
+#4 - men's and women's bmi percentage difference
+
+print("Men vs women BMI in highest population countries: \n")
+
+y1 = 2004-1980
+y2 = 2005-1980
+y3 = 2006-1980
+y4 = 2007-1980
+y5 = 2008-1980
+
+bmi_men_avg = round((bmi_men_dict['China'][y1]+bmi_men_dict['China'][y2]+bmi_men_dict['China'][y3]+\
+                bmi_men_dict['China'][y4]+bmi_men_dict['China'][y5])/5.0,2)
+
+bmi_women_avg = round((bmi_women_dict['China'][y1]+bmi_women_dict['China'][y2]+bmi_women_dict['China'][y3]+\
+                bmi_women_dict['China'][y4]+bmi_women_dict['China'][y5])/5.0,2)
+
+
+bmi_percent = round(((abs(bmi_women_avg - bmi_men_avg))/((bmi_women_avg + bmi_men_avg)/2.0)) * 100,2)
+
+print("*** China ***")
+print("Men: ",bmi_men_avg)
+print("Women: ",bmi_women_avg)
+print("Percent Differnce: ",bmi_percent)
+print("\n")
+
+
+bmi_men_avg = round((bmi_men_dict['India'][y1]+bmi_men_dict['India'][y2]+bmi_men_dict['India'][y3]+\
+                bmi_men_dict['India'][y4]+bmi_men_dict['India'][y5])/5.0,2)
+
+bmi_women_avg = round((bmi_women_dict['India'][y1]+bmi_women_dict['India'][y2]+bmi_women_dict['India'][y3]+\
+                bmi_women_dict['India'][y4]+bmi_women_dict['India'][y5])/5.0,2)
+
+
+bmi_percent = round(((abs(bmi_women_avg - bmi_men_avg))/((bmi_women_avg + bmi_men_avg)/2.0)) * 100,2)
+
+print("*** India ***")
+print("Men: ",bmi_men_avg)
+print("Women: ",bmi_women_avg)
+print("Percent Differnce: ",bmi_percent)
+print("\n")
+
+
+bmi_men_avg = round((bmi_men_dict['United States'][y1]+bmi_men_dict['United States'][y2]+bmi_men_dict['United States'][y3]+\
+                bmi_men_dict['United States'][y4]+bmi_men_dict['United States'][y5])/5.0,2)
+
+bmi_women_avg = round((bmi_women_dict['United States'][y1]+bmi_women_dict['United States'][y2]+bmi_women_dict['United States'][y3]+\
+                bmi_women_dict['United States'][y4]+bmi_women_dict['United States'][y5])/5.0,2)
+
+
+bmi_percent = round(((abs(bmi_women_avg - bmi_men_avg))/((bmi_women_avg + bmi_men_avg)/2.0)) * 100,2)
+
+print("*** united States ***")
+print("Men: ",bmi_men_avg)
+print("Women: ",bmi_women_avg)
+print("Percent Differnce: ",bmi_percent)
+print("\n")
+
+
+#5 - life exp plot
+
+while True:
+
+    country_name = str(input("Enter the country to visualize life expectancy data: "))
+    f = 0
+    for i in range(0,186):
+        if str(life.loc[i][0].lower()) == str(country_name.lower()):
+            name = life.loc[i][0]
+            f = 1
+            break
+    if f == 0:
+        print("<error> '"+country_name+"' is not a valid country.")
+        continue
+    else:
+        x = list(life.loc[i][1:])
+        y = [i for i in range(1980,2009)]
+
+        print("Plot for '"+str(life.loc[i][0])+"' opens in a new window.\n")
+
+        plt.plot(x,y)
+        plt.xlabel('life expectancy')
+        plt.ylabel('years')
+        plt.show()
+        break
+
+#6 - plot graph bmi world wide to life exp
+
+y_bmi_world = []
+
+for i in range(0,29):
+    x = 0
+    for j in range(0,186):
+        x += bmi_all.iloc[i][j]
+    x = x/186.0
+    y_bmi_world.append(x)
+
+y_life = []
+
+for i in range(1,30):
+    x=0
+    for j in range(0,186):
+        x += life.iloc[j][i]
+    x = x/186.0
+    y_life.append(x)
+
+print("Correlation opens in a new window.\n")
+
+x = [i for i in range(1980,2009)]
+
+fig = plt.figure()
+ax1 = fig.add_subplot()
+ax1.set_xlabel('Years')
+ax1.plot(x, y_life, 'b*-')
+ax1.tick_params(axis = 'y', labelcolor = 'b')
+ax1.set_ylabel('Avg Life Exp', color = 'b')
+
+ax2 = ax1.twinx()
+ax2.plot(x, y_bmi_world, 'ro-')
+ax2.tick_params(axis = 'y', labelcolor = 'r')
+ax2.set_ylabel('avg world wide bmi', color = 'r')
+plt.show()
---
+++ try.py
@@ -0,0 +1,11 @@
+d = {
+	'a':1, 'b': 2, 'c': 3,
+}
+
+d1 = {
+	'a1':1, 'b1': 2, 'c1': 3,
+}
+
+for i, j in d.items() and i1, j1 in d1.items():
+	print(i,j)
+	print(i1, j1)
